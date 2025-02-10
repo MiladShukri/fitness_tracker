8379de0d61143e00a8f588de0c1d15b1e09283d4
MiladShukri <Milad101@hotmail.co.uk>
2025-02-10 14:17:55 +0000

    edit config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+import { FlatCompat } from "@eslint/eslintrc";
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+    },
+  }),
 ];
+export default eslintConfig;
